d351c4a45ab26924a99ed0c10655b607cb1be7cd
pkmmollman <pkmollman@gmail.com>
2024-11-03 08:50:00 -0800

    moving this I guess

--- MineCartRally1/MineCartRally1.ino
+++ MineCartRally1/MineCartRally1.ino
@@ -10,6 +10,11 @@ const int IRSensor4 = 9;  // IR Sensor 4 connected to digital pin 5
 const int IRSensor5 = 10;  // IR Sensor 5 connected to digital pin 6
 const int IRSensor6 = 11;  // IR Sensor 6 connected to digital pin 7
 
+// dip in proper order
+const int SWITCHES[] = { 4, 7, 8 };
+
+
+
 // Array to store the IR sensor pins
 int IRSensors[] = {IRSensor1, IRSensor2, IRSensor3, IRSensor4, IRSensor5, IRSensor6};
 
@@ -21,6 +26,10 @@ void setup() {
   // Initialize serial communication for printing to the console
   Serial.begin(9600);
 
+  for (int i = 0; i < 3; i++) {
+    pinMode(SWITCHES[i], INPUT_PULLUP);
+  }
+
   // Set the IR sensor pins as input
   for (int i = 0; i < 6; i++) {
     pinMode(IRSensors[i], INPUT);
@@ -31,6 +40,18 @@ void loop() {
   // Get the current time
   unsigned long currentTime = millis();
 
+  int flag_state = 0;
+
+  for (int i = 0; i < 3; i++) {
+    int dip_state = digitalRead(SWITCHES[i]);
+    flag_state = flag_state + (dip_state << i);
+    // Serial.print("Dip: ");
+    // Serial.print(i);
+    // Serial.print(" State: ");
+    // Serial.println(dip_state);
+  }
+  // Serial.println(flag_state);
+
   // Check each IR sensor for detection
   for (int i = 0; i < 6; i++) {
     // If the IR sensor detects the ball (assuming LOW indicates detection) and the cooldown period has passed
@@ -39,8 +60,19 @@ void loop() {
       Serial.print(i + 1);
       Serial.println(": DETECTED");
 
+      send_score_event(flag_state, i+1);
+
       // Update the last detection time for the current sensor
       lastDetectionTime[i] = currentTime;
     }
   }
 }
+
+void send_message(String messageType, String messageContent) {
+  Serial.println(messageType + ";" + messageContent + ";" + messageType);
+}
+
+void send_score_event(int playerId, int targetId) {
+  send_message("PLAYER_SCORE", "player_id:"+String(playerId)+";"+"targetId:"+String(targetId));
+}
+  
